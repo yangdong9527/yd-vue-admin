8b733fd77d4db2b729a697b586da9c620ecfe485
杨东 <yd@unifound.net>
2020-04-12 20:16:51 +0800

    登录页重写

--- src/views/login/components/SignIn.vue
+++ src/views/login/components/SignIn.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="sign-wrap">
-    <el-form ref="form" :model="form" label-width="60px" size="small">
-      <el-form-item label="邮 箱 :">
-        <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+    <h3>用户登录</h3>
+    <el-form ref="form" :model="form" size="small" :rules="rules">
+      <el-form-item label="邮 箱 :" prop="email">
+        <el-input v-model="form.email" placeholder="请输入邮箱" @keyup.enter.native="login"></el-input>
       </el-form-item>
-      <el-form-item label="密 码 :">
-        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      <el-form-item label="密 码 :" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码" @keyup.enter.native="login"></el-input>
       </el-form-item>
-      <el-button size="small" type="primary" class="btn" @click="login">登 录</el-button>
+      <p><span @click="change">没有账号,去注册</span></p>
+      <el-button class="btn" type="primary" :loading="loading" @click="login">{{loading ? '登录中...' : '登 录'}}</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
+import { loginAPI } from "@/api/login.js";
 export default {
   name: "SignIn",
   data() {
@@ -20,21 +23,65 @@ export default {
       form: {
         email: "",
         password: ""
-      }
+      },
+      rules: {
+        email: { type: 'email', required: true, message: '请输入邮箱', trigger: 'blur' },
+        password: { required: true, message: '请输入登录密码', trigger: 'blur' }
+      },
+      loading: false
     };
   },
   methods: {
-    login() {
-      this.$emit('login', this.form)
+    login(params) {
+      this.$refs.form.validate(async bool => {
+        if(bool) {
+          this.loading = true
+          try {
+            const res = await this.$store.dispatch('Login', this.form)
+            this.$notify.success({
+              title: res.message
+            })
+            this.$router.push({name: 'home'})
+          } catch (error) {
+            console.log(error)
+          }
+          this.loading = false
+        }
+      })
+      
+    },
+    change() {
+      this.$emit('change', 'Register')
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-.sign-wrap .btn {
-  position: absolute;
-  bottom: 20px;
-  width: 310px;
+.sign-wrap {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 6px;
+  overflow: hidden;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  padding: 30px 20px;
+  h3 {
+    margin-bottom: 15px;
+  }
+  .btn {
+    margin-top: 10px;
+    width: 100%;
+  }
+  p {
+    margin-top: 10px;
+    margin-bottom: 5px;
+    font-size: 12px;
+    text-align: right;
+    span {
+      color: #2878ff;
+      cursor: pointer;
+    }
+  }
 }
 </style>
--- src/views/login/components/register.vue
+++ src/views/login/components/register.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="register-wrap">
-    <el-form ref="form" :model="form" label-width="60px" size="small">
-      <el-form-item label="邮 箱 :">
-        <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+    <h3>用户注册</h3>
+    <el-form ref="form" :model="form" size="small" :rules="rules">
+      <el-form-item label="邮 箱 :" prop="email">
+        <el-input v-model="form.email" placeholder="请输入邮箱" @keyup.enter.native="register"></el-input>
       </el-form-item>
-      <el-form-item label="昵 称 :">
-        <el-input v-model="form.nickName" placeholder="请输入昵称"></el-input>
+      <el-form-item label="昵 称 :" prop="nickName">
+        <el-input v-model="form.nickName" placeholder="请输入昵称" @keyup.enter.native="register"></el-input>
       </el-form-item>
-      <el-form-item label="密 码 :">
-        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      <el-form-item label="密 码 :" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码" @keyup.enter.native="register"></el-input>
       </el-form-item>
-      <el-button class="btn" size="small" type="primary" @click="register">注 册</el-button>
+      <p class="to-login" ><span @click="change">去登录</span></p>
+      <el-button class="btn" type="primary" @click="register" :loading="loading">{{loading ? '注册中...' : '注 册'}}</el-button>
     </el-form>
+    <div class="tip">
+      <p>提示: 该界面注册用户默认给予游客角色,仅拥有所有查看权限</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { registerAPI } from "@/api/login.js";
 export default {
   name: "register",
   data() {
@@ -24,21 +30,71 @@ export default {
         email: "",
         password: "",
         nickName: ''
-      }
+      },
+      rules: {
+        email: { type: 'email', required: true, message: '请输入邮箱', trigger: 'blur' },
+        nickName: { required: true, message: '请输入昵称', trigger: 'blur' },
+        password: { required: true, message: '请输入密码', trigger: 'blur' }
+      },
+      loading: false
     };
   },
   methods: {
     register() {
-      this.$emit('register', this.form)
+      this.$refs.form.validate(async bool => {
+        if(bool) {
+          this.loading = true
+          try {
+            const res = await registerAPI(this.form)
+            this.$notify.success({
+              title: res.message
+            })
+            this.currCompoent = 'SignIn'
+          } catch (error) {
+            console.log(error)
+          }
+          this.loading = false
+        }
+      })
+      
+    },
+    change() {
+      this.$emit('change', 'SignIn')
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-.register-wrap .btn {
-  position: absolute;
-  bottom: 20px;
-  width: 310px;
+.register-wrap {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 6px;
+  overflow: hidden;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  padding: 30px 20px;
+  h3 {
+    margin-bottom: 15px;
+  }
+  .btn {
+    margin-top: 10px;
+    width: 100%;
+  }
+  .tip {
+    margin-top: 10px;
+    font-size: 12px;
+    text-align: center;
+    
+  }
+  .to-login {
+    font-size: 12px;
+    color: #2878ff;
+    text-align: right;
+    margin-top: 5px;
+    span {
+      cursor: pointer;
+    }
+  }
 }
 </style>
